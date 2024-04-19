62bd4e6f09549ab13f603f1b6b490cfda97ea59e
Lucas Montecino <lucasmontecino95@gmail.com>
2024-04-19 13:36:02 -0300

    scroll to top functionallity added

--- homeworks/05-css-advanced/index.js
+++ homeworks/05-css-advanced/index.js
@@ -1,5 +1,6 @@
 const wrapper = document.querySelector(".wrapper");
 const buttonElement = document.querySelector(".header__button");
+const buttonTop = document.querySelector(".scroll__button");
 
 let theme = localStorage.getItem("theme") || "light";
 
@@ -7,6 +8,15 @@ function changeTheme() {
   theme = theme === "light" ? "dark" : "light";
   wrapper.setAttribute("data-theme", theme);
   localStorage.setItem("theme", theme);
+}
+
+function scrollWindow() {
+  if (window.scrollY !== 0) {
+    setTimeout(() => {
+      window.scrollTo(0, window.scrollY - 50);
+      scrollWindow();
+    }, 15);
+  }
 }
 
 buttonElement.addEventListener("click", () => {
@@ -17,6 +27,8 @@ buttonElement.addEventListener("click", () => {
     wrapper.classList.remove("theme-transition");
   }, 300);
 });
+
+buttonTop.addEventListener("click", scrollWindow);
 
 window.addEventListener("load", () => {
   wrapper.setAttribute("data-theme", theme);
