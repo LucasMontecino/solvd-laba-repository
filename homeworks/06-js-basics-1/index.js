c1644994cdad079a35fbfddba25f618c67bfcdba
Lucas Montecino <lucasmontecino95@gmail.com>
2024-04-30 14:52:18 -0300

    finish 1 to 4 tasks

--- homeworks/06-js-basics-1/index.js
+++ homeworks/06-js-basics-1/index.js
@@ -1,1 +1,42 @@
-console.log("hello world");
+// 1 http://www.codewars.com/kata/opposite-number
+
+function opposite(number) {
+  return number && number !== 0
+    ? -number
+    : number === 0
+    ? number
+    : "Please enter a number";
+}
+
+// 2 http://www.codewars.com/kata/basic-mathematical-operations
+
+function basicOp(operation, value1, value2) {
+  let result;
+
+  switch (operation) {
+    case "+":
+      result = value1 + value2;
+      break;
+    case "-":
+      result = value1 - value2;
+      break;
+    case "*":
+      result = value1 * value2;
+      break;
+    case "/":
+      result = value1 / value2;
+      break;
+    default:
+      result = `If you don't give a valid operation you've never seen a result.`;
+  }
+
+  return result;
+}
+
+// 3 http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
+
+function printArray(array) {
+  return array.join(",");
+}
+
+// 4 http://www.codewars.com/kata/transportation-on-vacation
