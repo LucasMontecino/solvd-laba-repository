f5b2315200333d4cb9e344b40790390e5fab0505
Lucas Montecino <lucasmontecino95@gmail.com>
2024-05-01 11:25:47 -0300

    finish 4 and 5 task

--- homeworks/06-js-basics-1/index.js
+++ homeworks/06-js-basics-1/index.js
@@ -1,4 +1,4 @@
-// 1 http://www.codewars.com/kata/opposite-number
+// 1 https://www.codewars.com/kata/opposite-number
 
 function opposite(number) {
   return number && number !== 0
@@ -8,7 +8,7 @@ function opposite(number) {
     : "Please enter a number";
 }
 
-// 2 http://www.codewars.com/kata/basic-mathematical-operations
+// 2 https://www.codewars.com/kata/basic-mathematical-operations
 
 function basicOp(operation, value1, value2) {
   let result;
@@ -33,10 +33,77 @@ function basicOp(operation, value1, value2) {
   return result;
 }
 
-// 3 http://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
+// 3 https://www.codewars.com/kata/printing-array-elements-with-comma-delimiters
 
 function printArray(array) {
   return array.join(",");
 }
 
-// 4 http://www.codewars.com/kata/transportation-on-vacation
+// 4 https://www.codewars.com/kata/transportation-on-vacation
+
+function rentalCarCost(d) {
+  let result = d * 40;
+  return d >= 7 ? (result -= 50) : d >= 3 ? (result -= 20) : result;
+}
+
+// 5 https://www.codewars.com/kata/calculating-with-functions
+
+function expression(number, operation) {
+  if (!operation) return number;
+  return operation(number);
+}
+
+function zero(operation) {
+  return expression(0, operation);
+}
+function one(operation) {
+  return expression(1, operation);
+}
+
+function two(operation) {
+  return expression(2, operation);
+}
+
+function three(operation) {
+  return expression(3, operation);
+}
+
+function four(operation) {
+  return expression(4, operation);
+}
+
+function five(operation) {
+  return expression(5, operation);
+}
+
+function six(operation) {
+  return expression(6, operation);
+}
+
+function seven(operation) {
+  return expression(7, operation);
+}
+
+function eight(operation) {
+  return expression(8, operation);
+}
+
+function nine(operation) {
+  return expression(9, operation);
+}
+
+function plus(x) {
+  return (y) => y + x;
+}
+
+function minus(x) {
+  return (y) => y - x;
+}
+
+function times(x) {
+  return (y) => x * y;
+}
+
+function dividedBy(x) {
+  return (y) => Math.floor(y / x);
+}
