58b28136f57816f9d249ebf8d9b16fde7aeccff4
Lucas Montecino <lucasmontecino95@gmail.com>
2024-05-06 20:19:50 -0300

    finish advanced 1

--- homeworks/07-js-basics-2/index.js
+++ homeworks/07-js-basics-2/index.js
@@ -157,3 +157,40 @@ function sortArray(array) {
     }
   });
 }
+
+// advanced 1 https://www.codewars.com/kata/515bb423de843ea99400000a
+
+class PaginationHelper {
+  constructor(collection, itemsPerPage) {
+    // The constructor takes in an array of items and a integer indicating how many
+    // items fit within a single page
+    this.collection = collection;
+    this.itemsPerPage = itemsPerPage;
+  }
+  itemCount() {
+    // returns the number of items within the entire collection
+    return this.collection.length;
+  }
+  pageCount() {
+    // returns the number of pages
+    return Math.ceil(this.itemCount() / this.itemsPerPage);
+  }
+  pageItemCount(pageIndex) {
+    // returns the number of items on the current page. page_index is zero based.
+    // this method should return -1 for pageIndex values that are out of range
+    //
+    if (pageIndex > this.pageCount() - 1 || pageIndex < 0) return -1;
+    let currentPage = this.collection.slice(
+      this.itemsPerPage * pageIndex,
+      this.itemsPerPage + this.itemsPerPage * pageIndex
+    );
+    return currentPage.length;
+  }
+  pageIndex(itemIndex) {
+    // determines what page an item is on. Zero based indexes
+    // this method should return -1 for itemIndex values that are out of range
+    if (itemIndex > this.itemCount() - 1 || itemIndex < 0) return -1;
+
+    return Math.ceil((itemIndex + 1) / this.itemsPerPage) - 1;
+  }
+}
