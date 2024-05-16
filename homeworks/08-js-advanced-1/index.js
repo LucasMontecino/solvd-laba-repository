2490f0888307d961ef0d78f2a55e5d48a3119c8e
Lucas Montecino <lucasmontecino95@gmail.com>
2024-05-16 18:21:30 -0300

    finish: task 4, 5 and 6

--- homeworks/08-js-advanced-1/index.js
+++ homeworks/08-js-advanced-1/index.js
@@ -57,3 +57,73 @@ function offset(date) {
 }
 
 // 4. Random Dates
+
+function randomDate(startDate, endDate) {
+  const startTimestamp = new Date(startDate).getTime();
+  const endTimestamp = new Date(endDate).getTime();
+
+  if (startTimestamp > endTimestamp) {
+    throw new Error("The start date must be before the end date");
+  }
+
+  const randomTimestamp =
+    Math.random() * (endTimestamp - startTimestamp) + startTimestamp;
+
+  return new Date(randomTimestamp);
+}
+
+// 5 https://www.codewars.com/kata/merged-objects
+
+function objConcat(o) {
+  let mergedObject = {};
+
+  o.forEach((obj) => {
+    Object.keys(obj).forEach((key) => {
+      mergedObject[key] = obj[key];
+    });
+  });
+
+  return mergedObject;
+}
+
+// 6 https://www.codewars.com/kata/547f1a8d4a437abdf800055c
+
+function NamedOne(first, last) {
+  let _firstName = first;
+  let _lastName = last;
+
+  Object.defineProperty(this, "firstName", {
+    get: function () {
+      return _firstName;
+    },
+    set: function (newFirstName) {
+      _firstName = newFirstName;
+      this.fullName = `${_firstName} ${_lastName}`;
+    },
+  });
+
+  Object.defineProperty(this, "lastName", {
+    get: function () {
+      return _lastName;
+    },
+    set: function (newLastName) {
+      _lastName = newLastName;
+      this.fullName = `${_firstName} ${_lastName}`;
+    },
+  });
+
+  Object.defineProperty(this, "fullName", {
+    get: function () {
+      return `${_firstName} ${_lastName}`;
+    },
+    set: function (newFullName) {
+      const parts = newFullName.split(" ");
+      if (parts.length === 2) {
+        _firstName = parts[0];
+        _lastName = parts[1];
+      }
+    },
+  });
+
+  this.fullName = `${_firstName} ${_lastName}`;
+}
