5c5918aeea75da480e76cd36b22d40c6952b411e
Lucas Montecino <lucasmontecino95@gmail.com>
2024-05-30 13:02:13 -0300

    add: binary search engine

--- homeworks/12-data-structures-and-algorithms/index.js
+++ homeworks/12-data-structures-and-algorithms/index.js
@@ -4,10 +4,34 @@ const port = 3000;
 
 const app = express();
 
-function searchStraightEngine(array, sku) {
+function compareValues(a, b) {
+  if (a > b) return 1;
+  if (a === b) return 0;
+  if (a < b) return -1;
+}
+
+function sortedElements(array, compareFn) {
+  return array.slice().sort(compareFn);
+}
+
+function straightSearchEngine(array, sku) {
   for (let i = 0; i < array.length; i++) {
     if (array[i].sku === sku) return array[i];
   }
+}
+
+function binarySearchEngine(array, sku) {
+  let left = 0;
+  let right = array.length - 1;
+
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (array[mid].sku === sku) return array[mid];
+    else if (array[mid].sku < sku) left = mid + 1;
+    else right = mid - 1;
+  }
+
+  return null;
 }
 
 app.use(express.json());
@@ -16,11 +40,32 @@ app.get("/data", (req, res) => {
   res.status(200).json(data);
 });
 
-app.get("/data/:id", (req, res) => {
+app.get("/data/sorted", (req, res) => {
+  const sortData = sortedElements(data, (a, b) =>
+    compareValues(a.name, b.name)
+  );
+  res.status(200).json(sortData);
+});
+
+app.get("/data/binarySearch/:id", (req, res) => {
+  const { id } = req.params;
+  const sortData = sortedElements(data, (a, b) => compareValues(a.sku, b.sku));
+  try {
+    const product = binarySearchEngine(sortData, id);
+
+    if (!product) throw Error(`There is no product with sku: ${id}`);
+
+    return res.status(200).json(product);
+  } catch (error) {
+    return res.status(404).json({ message: error.message });
+  }
+});
+
+app.get("/data/straightSearch/:id", (req, res) => {
   const { id } = req.params;
 
   try {
-    const product = searchStraightEngine(data, id);
+    const product = straightSearchEngine(data, id);
 
     if (!product) throw Error(`There is no product with sku: ${id}`);
 
